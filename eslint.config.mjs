c131eb51e048fd4ed6daf42fb3edc1169228667d
wataru endo <w4.3nd0@watarunoMacBook-Air.local>
2025-04-08 21:57:16 +0900

    other: Add linter and prettier (#3)

---
+++ eslint.config.mjs
@@ -0,0 +1,32 @@
+import { defineConfig } from 'eslint/config';
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    rules: {
+      'prettier/prettier': [
+        'error',
+        {
+          semi: true,
+          singleQuote: true,
+          tabWidth: 2,
+          trailingComma: 'es5',
+          bracketSpacing: true,
+          printWidth: 80,
+        },
+      ],
+    },
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    languageOptions: { globals: globals.browser },
+  },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+]);
